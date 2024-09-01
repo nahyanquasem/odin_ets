a225cb47f720858a25b414a0ec38644ad6f66a45
Nahyan Quasem <nahyan.quasem+github@gmail.com>
2024-09-01 11:14:08 +0600

    Adds grid to the html
    Creates a grid using JS DOM manipulation.
    Addes cell names to each grid.

--- ets.js
+++ ets.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+const gridBox = document.querySelector('#gridBox');
+
+
+function drawGrid(gridSize){
+
+    for (let i = 0; i<gridSize; i++){
+        const newDiv = document.createElement('div');
+        newDiv.id = 'gridCell';
+        newDiv.textContent = `Cell ${i}`;
+        gridBox.append(newDiv);
+    }
+}
+
+
+
+drawGrid(16);
