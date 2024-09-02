eb171cd2cf1e136dd882ff46871b11995f7a846e
Nahyan Quasem <nahyan.quasem+github@gmail.com>
2024-09-02 21:42:52 +0600

    COLORS!!!! AAAAHHHH
    Cells change background color to random values on mouseover.

--- ets.js
+++ ets.js
@@ -11,6 +11,30 @@ function drawGrid(gridSize){
     }
 }
 
+function randomColorGenerator(){
+
+    let colorDepth = 2**8;
+
+    const colors = [];
+
+    for(let i = 0; i<3; i++){
+        colors.push((Math.floor(Math.random() *  colorDepth)));
+    }
+
+    return colors;
+
+}
+
+function changeBackgroundColorRandomly(cell){
+    let colors = randomColorGenerator();
+
+    let text = `RED: ${colors[0]} GREEN: ${colors[1]} BLUE: ${colors[2]}`;
+    // colorValuesPage.textContent = text;
+
+    cell.style.cssText = `background-color:rgb(${colors[0]} ${colors[1]} ${colors[2]})`;
+
+}
+
 drawGrid(16);
 
 const gridCell = document.querySelectorAll('#gridCell');
@@ -19,11 +43,13 @@ const gridCell = document.querySelectorAll('#gridCell');
 gridCell.forEach((cell)=>{
 
     cell.addEventListener('mouseenter', ()=>{
+        changeBackgroundColorRandomly(cell);
         console.log(`Enter ${cell.textContent}`);
 
     });
 
     cell.addEventListener('mouseleave', ()=>{
+        changeBackgroundColorRandomly(cell);
         console.log(`Leave ${cell.textContent}`);
 
     });
