9d37a624cf82aff334e3f53205fd4bef00fde591
Nahyan Quasem <nahyan.quasem+github@gmail.com>
2024-09-03 09:51:50 +0600

    Gets ready to introduce more features
    Adds control div for more application features/controls.
    Introduces a button to reset all cells to 255,255,255.

--- ets.js
+++ ets.js
@@ -1,9 +1,6 @@
 const gridBox = document.querySelector('#gridBox');
-const body = document.querySelector('body');
-
-
 function drawGrid(gridSize){
-
+    
     for (let i = 0; i<gridSize*gridSize; i++){
         const newDiv = document.createElement('div');
         newDiv.id = 'gridCell';
@@ -13,44 +10,45 @@ function drawGrid(gridSize){
 }
 
 function randomColorGenerator(){
-
+    
     let colorDepth = 2**8;
-
+    
     const colors = [];
-
+    
     for(let i = 0; i<3; i++){
         colors.push((Math.floor(Math.random() *  colorDepth)));
     }
-
+    
     return colors;
-
+    
 }
 
 function changeBackgroundColorRandomly(cell){
     let colors = randomColorGenerator();
-
+    
     let text = `RED: ${colors[0]} GREEN: ${colors[1]} BLUE: ${colors[2]}`;
     cell.style.cssText = `background-color:rgb(${colors[0]} ${colors[1]} ${colors[2]})`;
-
+    
     return text;
-
+    
 }
 
 drawGrid(8);
 
-const gridCell = document.querySelectorAll('#gridCell');
 
-body.addEventListener('click', ()=>{
-
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', ()=>{
+    
     gridCell.forEach((cell) => {
-
+        
         cell.style.cssText = 'background-color:rgb(255 255 255)';
-
+        
     });
-
+    
 });
 
 
+const gridCell = document.querySelectorAll('#gridCell');
 gridCell.forEach((cell)=>{
 
     cell.addEventListener('mouseenter', ()=>{
